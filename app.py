b56b3bcfe9c39c108ead279281f19430ce21f081
mdar4 <darasilva2391@gmail.com>
2021-07-11 16:47:52 -0300

    criando rota

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 class Musica:
     def __init__(self, nome, artista, link):
         self.nome = nome
         self.artista = artista
         self.link = link
-
-# @app.route('/')
