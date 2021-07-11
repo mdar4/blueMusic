c6b55a7bcd32ffce3d787d79c9407332e3854f07
mdar4 <darasilva2391@gmail.com>
2021-07-11 18:59:57 -0300

    criando e estilizando nav

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template, request, redirect
-
 app = Flask(__name__)
 
+# Criando rota 
 @app.route('/')
+# Linkando ao index.html
 def index():
     return render_template('index.html')
+
+# Verificação de Debug, irá recarregar a cada alteração do programa
+if __name__ == '__main__':
+    app.run(debug=True)
 class Musica:
     def __init__(self, nome, artista, link):
         self.nome = nome
